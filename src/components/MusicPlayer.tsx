9109ed459d7b6159c812151b31c1a86ef5c05878
dharma mudita <dharmamudita404@gmail.com>
2026-02-21 23:24:41 +0700

    Fix: increase MusicPlayer z-index to appear above countdown/splash overlays

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -82,7 +82,7 @@ export default function MusicPlayer() {
     };
 
     return (
-        <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-3">
+        <div className="fixed bottom-6 right-6 z-[10001] flex flex-col items-end gap-3">
             {/* Expanded player */}
             <AnimatePresence>
                 {isExpanded && (
